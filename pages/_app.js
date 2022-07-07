6db9f54fa81b922a29530a4c7680a844bdb64bdb
huycan <vohuy21032001@gmail.com>
2022-07-07 15:01:46 +0700

    update UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {Layout} from '../components';
+import '../styles/globals.css';
+
+function MyApp({Component, pageProps}) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
-export default MyApp
+export default MyApp;
